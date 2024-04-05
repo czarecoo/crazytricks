13fda9590e62c2ed8c0b9f4479ac6a245cefc153
Czareg <czarecoo@gmail.com>
2024-04-05 17:45:13 +0200

    Added more crazy tricks

---
+++ src/main/java/com/czareg/Crazy12.java
@@ -0,0 +1,13 @@
+package com.czareg;
+
+public class Crazy12 {
+
+    public static void main(String[] args) {
+        int i = 2;
+
+        https://google.com
+        {
+            int j = 5;
+        }
+    }
+}
